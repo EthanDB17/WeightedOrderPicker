385896f7c9390caf60e431d241500ea0b3fab013
Ethan Borrowman <ethan_borrowman@hotmail.com>
2024-08-18 22:20:50 -0500

    Initial Commit

---
+++ WeightedOrderPicker/WeightedOrderPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightedOrderPickerApp.swift
+//  WeightedOrderPicker
+//
+//  Created by Ethan Borrowman on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeightedOrderPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeightedOrderPickerTests/WeightedOrderPickerTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeightedOrderPickerTests.swift
+//  WeightedOrderPickerTests
+//
+//  Created by Ethan Borrowman on 8/18/24.
+//
+
+import Testing
+
+struct WeightedOrderPickerTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
